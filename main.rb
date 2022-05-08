0a6a2ca0a47a55e65191abd118781c02d317a01a
WireHallMedic <michaelwidler@yahoo.com>
2022-05-08 16:36:34 -0700

    don't like it, revert to original

--- main.rb
+++ main.rb
@@ -7,29 +7,3 @@ def draw_button(label_text, x, y, foreground_color, is_dark_mode)
       paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
     end
 end
-
-class ScreenMode
-    def draw_button(label_text, x, y, foreground_color)
-        raise "draw_button() cannot be called from parent class"
-    end
-
-    def dark()
-        new DarkMode
-    end
-
-    def light()
-        new LightMode
-    end
-end
-
-class DarkMode < ScreenMode
-    def draw_button(label_text, x, y, foreground_color)
-        paint(label_text, x, y, foreground_color - 10, '#111111')
-    end
-end
-
-class LightMode < ScreenMode
-    def draw_button(label_text, x, y, foreground_color)
-        paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
-    end
-end
